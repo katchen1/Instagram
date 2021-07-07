4bc2c7e5af130fb83d42dfbdf66e220e107cc888
katchen1 <katherinechen139@gmail.com>
2021-07-07 01:28:15 -0700

    Styled the login page to look like the real Instagram login page

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.instagram;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
