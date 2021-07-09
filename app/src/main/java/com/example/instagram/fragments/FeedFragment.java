519fca85589d9e6bfbb92e4ef78620962ded0b18
katchen1 <katherinechen139@gmail.com>
2021-07-08 18:36:08 -0700

    FeedFragment and ProfileFragment are different enough, so no need to extend from PostsFragment

--- app/src/main/java/com/example/instagram/fragments/FeedFragment.java
+++ app/src/main/java/com/example/instagram/fragments/FeedFragment.java
@@ -1,18 +1,58 @@
 package com.example.instagram.fragments;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import com.example.instagram.EndlessRecyclerViewScrollListener;
+import com.example.instagram.databinding.FragmentFeedBinding;
 import com.example.instagram.models.Post;
 import com.example.instagram.adapters.PostsAdapter;
 import com.parse.ParseQuery;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FeedFragment extends PostsFragment {
+public class FeedFragment extends Fragment {
 
+    public static final String TAG = "FeedFragment";
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+    FragmentFeedBinding binding;
     EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        binding = FragmentFeedBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize the array that will hold posts and create a PostsAdapter
+        allPosts = new ArrayList<>();
+        setLayoutManager();
+        binding.rvPosts.setAdapter(adapter);
+        queryPosts(0);
+
+        // Setup refresh listener which triggers new data loading
+        binding.swipeContainer.setOnRefreshListener(() -> {
+            allPosts.clear();
+            queryPosts(0);
+            binding.swipeContainer.setRefreshing(false);
+        });
+
+        // Configure the refreshing colors
+        binding.swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright);
+    }
+
     protected void setLayoutManager() {
         // Set up adapter and layout of recycler view
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
@@ -29,7 +69,6 @@ public class FeedFragment extends PostsFragment {
         binding.rvPosts.addOnScrollListener(scrollListener);
     }
 
-    @Override
     public void queryPosts(int skip) {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class); // specify type of data
         query.include(Post.KEY_USER); // include data referred by user key
--- app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,19 +1,59 @@
 package com.example.instagram.fragments;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.instagram.EndlessRecyclerViewScrollListener;
 import com.example.instagram.adapters.PostsAdapter;
+import com.example.instagram.databinding.FragmentProfileBinding;
 import com.example.instagram.models.Post;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ProfileFragment extends PostsFragment {
+public class ProfileFragment extends Fragment {
 
+    public static final String TAG = "ProfileFragment";
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+    FragmentProfileBinding binding;
     EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize the array that will hold posts and create a PostsAdapter
+        allPosts = new ArrayList<>();
+        setLayoutManager();
+        binding.rvPosts.setAdapter(adapter);
+        queryPosts(0);
+
+        // Setup refresh listener which triggers new data loading
+        binding.swipeContainer.setOnRefreshListener(() -> {
+            allPosts.clear();
+            queryPosts(0);
+            binding.swipeContainer.setRefreshing(false);
+        });
+
+        // Configure the refreshing colors
+        binding.swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright);
+    }
+
     protected void setLayoutManager() {
         // Set up adapter and layout of recycler view
         GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
@@ -30,7 +70,6 @@ public class ProfileFragment extends PostsFragment {
         binding.rvPosts.addOnScrollListener(scrollListener);
     }
 
-    @Override
     public void queryPosts(int skip) {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class); // specify type of data
         query.include(Post.KEY_USER); // include data referred by user key
